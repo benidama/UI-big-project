847a06479dd3eecff1ef5561f5753cdbe455365b
benidama <benidama02@gmail.com>
2024-12-29 20:58:29 +0000

    React-vite and tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl text-blue-800">Vite + React</h1>
+      <p>
+        I want to learn more about vite react, because it is a very good
+        framework in javascript especial in UI and mobile app development.
+      </p>
+    </>
+  );
+}
+
+export default App;
