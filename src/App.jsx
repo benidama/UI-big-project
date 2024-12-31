cb7fbb3400427fba261774b6fda7040492bfd6c0
benidama <benidama02@gmail.com>
2024-12-31 23:10:46 +0000

    Starting my project

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,24 @@
 import React from "react";
 import "./App.css";
+// import Navbar from "./components/Navbar";
 
 function App() {
-  const logIn = false;
-  if (logIn) {
-    return <h2>Welcome my brother</h2>;
-  }
+  const logIn = true;
+
   return (
     <>
+      {/* <Navbar /> */}
       <h1 className="text-3xl text-blue-800">Vite + React</h1>
       <p className="fontFamily-sans">
         I want to learn more about vite react, because it is a very good
         framework in javascript especial in UI and mobile app development.
       </p>
-      <p>
-        {logIn ? (
-          <h2>{logIn}</h2>
-        ) : (
-          <h2 className="text-1xl text-blue-800">You are not allowed</h2>
-        )}
-      </p>
+
+      {logIn ? (
+        <h2 className="text-1xl text-blue-800">Welcome my brother</h2>
+      ) : (
+        <h2 className="text-1xl text-blue-800">You are not allowed</h2>
+      )}
     </>
   );
 }
