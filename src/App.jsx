2216be76c87cc08edf5c864584d237d04a5b298e
benidama <benidama02@gmail.com>
2025-01-05 18:11:31 +0000

    Adding card file

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,71 @@
-import React from "react";
-import "./App.css";
-// import Navbar from "./components/Navbar";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+import HomePage from "./pages/HomePage";
+import JobsPage from "./pages/JobsPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import JobPage, { jobLoader } from "./pages/JobPage";
+import AddJobPage from "./pages/AddJobPage";
+import EditJobPage from "./pages/EditJobPage";
 
-function App() {
-  const logIn = true;
+const App = () => {
+  // Add New Job
+  const addJob = async (newJob) => {
+    const res = await fetch("/api/jobs", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newJob),
+    });
+    return;
+  };
 
-  return (
-    <>
-      {/* <Navbar /> */}
-      <h1 className="text-3xl text-blue-800">Vite + React</h1>
-      <p className="fontFamily-sans">
-        I want to learn more about vite react, because it is a very good
-        framework in javascript especial in UI and mobile app development.
-      </p>
+  // Delete Job
+  const deleteJob = async (id) => {
+    const res = await fetch(`/api/jobs/${id}`, {
+      method: "DELETE",
+    });
+    return;
+  };
 
-      {logIn ? (
-        <h2 className="text-1xl text-blue-800">Welcome my brother</h2>
-      ) : (
-        <h2 className="text-1xl text-blue-800">You are not allowed</h2>
-      )}
-    </>
+  // Update Job
+  const updateJob = async (job) => {
+    const res = await fetch(`/api/jobs/${job.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(job),
+    });
+    return;
+  };
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/jobs" element={<JobsPage />} />
+        <Route path="/add-job" element={<AddJobPage addJobSubmit={addJob} />} />
+        <Route
+          path="/edit-job/:id"
+          element={<EditJobPage updateJobSubmit={updateJob} />}
+          loader={jobLoader}
+        />
+        <Route
+          path="/jobs/:id"
+          element={<JobPage deleteJob={deleteJob} />}
+          loader={jobLoader}
+        />
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    )
   );
-}
 
+  return <RouterProvider router={router} />;
+};
 export default App;
